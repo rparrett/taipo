fed89d4ed1a4b8f2d13dc6fb47ae09138b75db48
Rob Parrett <robparrett@gmail.com>
2021-01-13 09:10:41 -0700

    Cleanup

--- src/app_stages.rs
+++ src/app_stages.rs
@@ -1,4 +1,3 @@
-pub const POST_UPDATE_STAGE: &str = "app_post_update_stage";
 pub const AFTER_UPDATE: &str = "app_after_update";
 pub const AFTER_UPDATE_2: &str = "app_after_update_2";
 pub const AFTER_STATE_STAGE: &str = "app_after_state_stage";
