1e770d6688765487d0ccec71d30f8d23abadadba
Rob Parrett <robparrett@gmail.com>
2024-03-19 07:21:50 -0700

    Disable windows console in release builds

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// disable console on windows for release builds
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use action_panel::{ActionPanel, ActionPanelItemImage, ActionPanelPlugin};
 use atlas_loader::{AtlasImage, AtlasImageLoader};
 use bevy::{
